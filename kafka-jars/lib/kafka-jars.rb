b1760c7ab38f1e99a0f608cde718063a79ab8477
Mathias Söderberg <mathias.soederberg@gmail.com>
2015-02-08 22:23:17 +0100

    kafka 0.8.2.1

---
+++ kafka-jars/lib/kafka-jars.rb
@@ -0,0 +1,10 @@
+# encoding: utf-8
+
+require 'java'
+require 'scala-library-jars'
+require 'slf4j-jars'
+require 'zookeeper-jars'
+require 'kafka-clients-jars'
+
+
+Dir["#{File.expand_path('..', __FILE__)}/*.jar"].each { |jar| require(jar) }
